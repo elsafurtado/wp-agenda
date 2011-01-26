86c1f4493e95f181b2874958bb8480ac69e9665d
Alexandre Magno Teles Zimerer <alexmagno@corp.globo.com>
2011-01-26 01:36:55 -0200

    finally the custom url structure make load main template files

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,21 @@
 <?php 
-
+global $wpdb;
+global $wp_rewrite;
 remove_post_type_support('WPAgenda');
 
+$WPAgenda_delete_query  = "
+    DELETE wposts.* 
+    FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
+    WHERE wposts.ID = wpostmeta.post_id 
+    AND wpostmeta.meta_key = 'tag' 
+    AND wpostmeta.meta_value = 'email' 
+    AND wposts.post_status = 'publish' 
+    AND wposts.post_type = 'WPAgenda' 
+    ORDER BY wposts.post_date DESC
+ ";
+
+$wpdb->query($WPAgenda_delete_query);
+
+$wp_rewrite->flush_rules();
 
 ?>
