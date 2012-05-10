cb049124f5ce93f4b75b47e1c6b4c29358ec6405
alexandremagno <alexandremagno@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-10 15:34:01 +0000

    mainly files, initial class structure and a wi for add new event
    git-svn-id: http://plugins.svn.wordpress.org/wp-agenda/trunk@542471 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/agenda-locale.js
@@ -0,0 +1,24 @@
+/*
+ * Configuracoes de linguagem da agenda
+ *
+ */
+
+var agenda_locale = {
+	monthNames: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho','Julho','Agosto',
+	'Setembro','Outubro','Novembro','Dezembro'
+	],
+	monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+	dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','S&aacute;bado'],
+	dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+	header: { left:'prev', center:'title', right:'next' },
+	buttonText: {
+        prev: '&nbsp;&#9668;&nbsp;',      // left triangle
+        next: '&nbsp;&#9658;&nbsp;',      // right triangle
+        prevYear: '&nbsp;&lt;&lt;&nbsp;', // <<
+        nextYear: '&nbsp;&gt;&gt;&nbsp;', // >>
+        today: 'Data atual',
+        month: 'm&ecirc;s',
+        week: 'semana',
+        day: 'dia'
+	}
+};
